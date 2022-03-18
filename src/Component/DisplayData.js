ae44c440fa8f5197b83dd24a2122bd39a295b9dd
itslight <42795611+itslight@users.noreply.github.com>
2022-03-19 00:45:18 +0530

    Add files via upload

---
+++ src/Component/DisplayData.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Axios from 'axios';
+import { useState, useEffect } from 'react';
+import Card from './Card';
+import './DisplayData.css'
+import {useSelector} from "react-redux";
+
+export default function DisplayData() {
+    const [list, setList] = useState({});
+    const myState = useSelector((state) => state.changeTheUrl )
+    let url = myState;
+    console.log(url);
+      useEffect(() => {
+        Axios.get(url).then((response) => {
+          setList(response.data);
+        });
+    console.log();
+      }, [url]);
+
+    if(JSON.stringify(list)==="{}"){
+      return(
+        <h1>Data Loading</h1>
+      )
+    }
+    else{
+      const {data:{results}} = list;
+      return (
+        <div className='sheet'>
+         {
+            results.map(item => <Card hName= {item.name} img_link={item.thumbnail} comic={item.comics}/>)
+          }
+        </div>
+      )
+    }
+}
