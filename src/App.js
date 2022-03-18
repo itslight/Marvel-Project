e742adf973fa5436ad9256b87c47a48d172bb5a6
itslight <42795611+itslight@users.noreply.github.com>
2022-03-19 00:46:16 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Api from './Component/Api';
+import DisplayData from './Component/DisplayData';
 import SearchBar from './Component/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <SearchBar/>
-      <Api/>
+      <DisplayData/>
     </div>
   );
 }
